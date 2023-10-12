4615558c11fa483091e6737c0c77ad4dfca37338
이은정 <leeeunjeong@ieunjeong-ui-MacBookAir.local>
2023-10-12 10:56:53 +0900

    서버 만듬

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const http = reqire("http");
+const http = require("http");
 
-const server = http.createSever((req, res) => {});
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/play" });
+  res.write("향마루");
+  //설명한다.
+  res.end();
+});
 server.listen(3500, (err) => {
-  if (err) {
+  if (!err) {
     console.log("서버 안돌아감");
   } else {
     console.log("3500 포트에 돌아감 http:///localhost:3500");
